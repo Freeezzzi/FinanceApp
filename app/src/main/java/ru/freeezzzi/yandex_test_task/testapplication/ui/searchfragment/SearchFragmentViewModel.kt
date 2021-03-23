48fc8f7c701bb674351e57d6b6efb1a60eb4925e
Freeezzzi <60979041+Freeezzzi@users.noreply.github.com>
2021-03-23 20:26:51 +0300

    few changes in search fragment

--- app/src/main/java/ru/freeezzzi/yandex_test_task/testapplication/ui/searchfragment/SearchFragmentViewModel.kt
+++ app/src/main/java/ru/freeezzzi/yandex_test_task/testapplication/ui/searchfragment/SearchFragmentViewModel.kt
@@ -1,14 +1,24 @@
 package ru.freeezzzi.yandex_test_task.testapplication.ui.searchfragment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.github.terrakok.cicerone.Router
 import ru.freeezzzi.yandex_test_task.testapplication.data.local.FavoriteCompaniesDatabase
+import ru.freeezzzi.yandex_test_task.testapplication.domain.models.CompanyProfile
+import ru.freeezzzi.yandex_test_task.testapplication.domain.repositories.CompaniesRepository
+import ru.freeezzzi.yandex_test_task.testapplication.ui.ViewState
 import javax.inject.Inject
 
 class SearchFragmentViewModel @Inject constructor(
         private val router: Router,
-        val database: FavoriteCompaniesDatabase
+        private val database: FavoriteCompaniesDatabase,
+        private val companiesRepository: CompaniesRepository
 ) : ViewModel() {
+    private val mutableCompanies = MutableLiveData<ViewState<MutableList<CompanyProfile>, String?>>()
+
+    val companies: LiveData<ViewState<MutableList<CompanyProfile>, String?>> get() = mutableCompanies
+
 
     fun exitFragment() {
         router.exit()
